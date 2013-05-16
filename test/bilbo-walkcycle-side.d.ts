2659829985c4a93926e0099faadd8ef7b6ffa367
elsassph <philippe.elsass@gmail.com>
2013-05-16 16:54:30 +0200

    typescript output

---
+++ test/bilbo-walkcycle-side.d.ts
@@ -0,0 +1,169 @@
+/// <reference path="easeljs.d.ts" />
+
+module lib {
+
+	export class bilbowalkcycleside extends createjs.MovieClip {
+		static nominalBounds: createjs.Rectangle;
+		instance: bilbofootwalkcycle;
+		instance_1: bilbofootwalkcycle;
+		instance_2: biblobodywalkcycle;
+		instance_3: bilbofootwalkcycle;
+		instance_4: bilbofootwalkcycle;
+		instance_5: shadow;
+	}
+
+	export class shadow2 extends createjs.Container {
+		static nominalBounds: createjs.Rectangle;
+		shape: createjs.Shape;
+	}
+
+	export class bilbotail extends createjs.MovieClip {
+		static nominalBounds: createjs.Rectangle;
+		shape: createjs.Shape;
+		shape_1: createjs.Shape;
+		shape_2: createjs.Shape;
+		shape_3: createjs.Shape;
+		shape_4: createjs.Shape;
+		shape_5: createjs.Shape;
+		shape_6: createjs.Shape;
+		shape_7: createjs.Shape;
+		shape_8: createjs.Shape;
+		shape_9: createjs.Shape;
+		shape_10: createjs.Shape;
+		shape_11: createjs.Shape;
+		shape_12: createjs.Shape;
+		shape_13: createjs.Shape;
+		shape_14: createjs.Shape;
+		shape_15: createjs.Shape;
+		shape_16: createjs.Shape;
+		shape_17: createjs.Shape;
+		shape_18: createjs.Shape;
+		shape_19: createjs.Shape;
+		shape_20: createjs.Shape;
+		shape_21: createjs.Shape;
+		shape_22: createjs.Shape;
+		shape_23: createjs.Shape;
+		shape_24: createjs.Shape;
+		shape_25: createjs.Shape;
+		shape_26: createjs.Shape;
+		shape_27: createjs.Shape;
+		shape_28: createjs.Shape;
+		shape_29: createjs.Shape;
+		shape_30: createjs.Shape;
+		shape_31: createjs.Shape;
+		shape_32: createjs.Shape;
+		shape_33: createjs.Shape;
+		shape_34: createjs.Shape;
+	}
+
+	export class bilbopupil extends createjs.Container {
+		static nominalBounds: createjs.Rectangle;
+		shape: createjs.Shape;
+		shape_1: createjs.Shape;
+	}
+
+	export class bilbomouthint extends createjs.Container {
+		static nominalBounds: createjs.Rectangle;
+		shape: createjs.Shape;
+	}
+
+	export class bilbohat extends createjs.Container {
+		static nominalBounds: createjs.Rectangle;
+		shape: createjs.Shape;
+		shape_1: createjs.Shape;
+	}
+
+	export class bilbofoot extends createjs.MovieClip {
+		static nominalBounds: createjs.Rectangle;
+		shape: createjs.Shape;
+		shape_1: createjs.Shape;
+		shape_2: createjs.Shape;
+		shape_3: createjs.Shape;
+	}
+
+	export class bilboeyeball extends createjs.Container {
+		static nominalBounds: createjs.Rectangle;
+		shape: createjs.Shape;
+	}
+
+	export class bilboear extends createjs.Container {
+		static nominalBounds: createjs.Rectangle;
+		shape: createjs.Shape;
+	}
+
+	export class bilbobrow extends createjs.Container {
+		static nominalBounds: createjs.Rectangle;
+		shape: createjs.Shape;
+	}
+
+	export class shadow extends createjs.MovieClip {
+		static nominalBounds: createjs.Rectangle;
+		instance: shadow2;
+	}
+
+	export class bilbofootwalkcycle extends createjs.MovieClip {
+		static nominalBounds: createjs.Rectangle;
+		instance: bilbofoot;
+	}
+
+	export class bilboeyebuild extends createjs.Container {
+		static nominalBounds: createjs.Rectangle;
+		instance: bilbopupil;
+		instance_1: bilboeyeball;
+		shape: createjs.Shape;
+	}
+
+	export class bilbobody extends createjs.MovieClip {
+		static nominalBounds: createjs.Rectangle;
+		instance: bilboeyebuild;
+		instance_1: bilbobrow;
+		shape: createjs.Shape;
+		shape_1: createjs.Shape;
+		shape_2: createjs.Shape;
+		shape_3: createjs.Shape;
+		shape_4: createjs.Shape;
+		shape_5: createjs.Shape;
+		shape_6: createjs.Shape;
+		shape_7: createjs.Shape;
+		shape_8: createjs.Shape;
+		shape_9: createjs.Shape;
+		shape_10: createjs.Shape;
+		shape_11: createjs.Shape;
+		shape_12: createjs.Shape;
+		shape_13: createjs.Shape;
+		shape_14: createjs.Shape;
+		shape_15: createjs.Shape;
+		shape_16: createjs.Shape;
+		shape_17: createjs.Shape;
+		shape_18: createjs.Shape;
+		shape_19: createjs.Shape;
+		shape_20: createjs.Shape;
+		shape_21: createjs.Shape;
+		shape_22: createjs.Shape;
+		shape_23: createjs.Shape;
+		shape_24: createjs.Shape;
+		shape_25: createjs.Shape;
+		shape_26: createjs.Shape;
+		shape_27: createjs.Shape;
+		shape_28: createjs.Shape;
+		shape_29: createjs.Shape;
+		shape_30: createjs.Shape;
+		shape_31: createjs.Shape;
+		shape_32: createjs.Shape;
+		shape_33: createjs.Shape;
+		shape_34: createjs.Shape;
+		shape_35: createjs.Shape;
+		shape_36: createjs.Shape;
+		instance_2: bilbomouthint;
+	}
+
+	export class biblobodywalkcycle extends createjs.MovieClip {
+		static nominalBounds: createjs.Rectangle;
+		instance: bilboear;
+		instance_1: bilbohat;
+		instance_2: bilbobody;
+		instance_3: bilbotail;
+	}
+
+}
+
