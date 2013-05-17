d2350756bc49f7fa5a5963d7baea61125bd5e608
elsassph <philippe.elsass@gmail.com>
2013-05-17 13:54:01 +0200

    windows-compatible binary

--- test/node-test.sh
+++ test/node-test.sh
@@ -1,5 +1,5 @@
-node ../cmd.js haxe bilbo/bilbo-walkcycle-side.js > bilbo/BilboWalkcycle.hx
-node ../cmd.js typescript bilbo/bilbo-walkcycle-side.js > bilbo/bilbo-walkcycle-side.d.ts
+../bin/createjs-def haxe bilbo/bilbo-walkcycle-side.js > bilbo/BilboWalkcycle.hx
+../bin/createjs-def typescript bilbo/bilbo-walkcycle-side.js > bilbo/bilbo-walkcycle-side.d.ts
 
-node ../cmd.js haxe sea/Sea.js > sea/Sea.hx
-node ../cmd.js typescript sea/Sea.js > sea/Sea.d.ts
+../bin/createjs-def haxe sea/Sea.js > sea/Sea.hx
+../bin/createjs-def typescript sea/Sea.js > sea/Sea.d.ts
